c4718bd31727bf37f5e01e5c96231610905d1428
tsumitakaoru <Katherine@KT-MacBook-Air-2.local>
2021-11-01 16:29:30 -0400

    change text

--- test/headTest.js
+++ test/headTest.js
@@ -11,7 +11,7 @@ describe("#head", () => {
   it("returns 5 for [5,6,7]", () => {
     assert.strictEqual(head([5,6,7]), 5); 
   });
-  it("returns 5 for []", () => {
+  it("returns undefined for []", () => {
     assert.strictEqual(head([]), undefined); 
   });
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs]", () => {
